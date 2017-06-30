5dd4729774732ca68b7eaa84e39036b512f35f93
Paulo R. Kraemer <paulork10@gmail.com>
2017-06-30 15:46:43 -0300

    PRIMEIRO COMMIT

---
+++ src/main/java/br/com/paulork/fileutils/Test.java
@@ -0,0 +1,7 @@
+package br.com.paulork.fileutils;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(FileUtils.getHumanReadableFormat(56465));
+    }
+}
